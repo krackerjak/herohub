47d7a1006da1e540f5e5cab0817c12471b1100a8
Benjamin Reed <benofsun@gmail.com>
2025-10-22 13:48:52 -0700

    Update main.py
    added wonder woman

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 # Pet gallery data: image filenames must match those in the images/ folder
 gallery = [
     {"name": "Batman", "species": "Human", "image": "batman.jpg"},
-    {"name": "Robin", "species": "Human", "image": "robin.jpg"}
+    {"name": "Robin", "species": "Human", "image": "robin.jpg"},
+    {"name": "Wonder Woman", "species": "Human", "image": "wonderwoman.jpg"}
 ]
 
 @app.route('/')
